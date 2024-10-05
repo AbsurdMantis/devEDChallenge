e02ddad3ea70daa709c830e7033cc9eb498e440b
M.AB <raphaelbholmes@gmail.com>
2024-10-05 06:43:21 -0300

    auth service

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from './prisma.service';
+import { compare, genSalt, hash } from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private jwtService: JwtService,
+    private prisma: PrismaService
+  ) {}
+
+  async validateUser(email: string, pass: string): Promise<any> {
+    const user = await this.prisma.user.findUnique({ where: { email } });
+    if (user && await compare(pass, user.password_hash)) {
+      const { password_hash, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = { email: user.email, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+
+  async register(name:string, email: string, pass: string) {
+    const salt = await genSalt();
+    const hashedPassword = await hash(pass, salt);
+
+    const user = await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password_hash: hashedPassword,
+        status: true,
+        isAdmin: false,
+      },
+    });
+
+    const { password_hash, ...result } = user;
+    return result;
+  }
+}
