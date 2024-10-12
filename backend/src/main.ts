c8f46edf49560a6bde3f9069e852deae59b280c2
M.AB <raphaelbholmes@gmail.com>
2024-10-12 20:12:45 -0300

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,12 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   await app.listen(3000);
-  Logger.log(`Server started running onhttp://localhost:3000`, 'Bootstrap');
+  Logger.log(`Server started running on http://localhost:3000`, 'Bootstrap');
 }
   bootstrap();
