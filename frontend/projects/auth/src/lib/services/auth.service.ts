058c4c70f2ea22dcefce79ff368d4a522ebb3e76
M.AB <raphaelbholmes@gmail.com>
2024-10-17 17:03:27 -0300

    Update auth.service.ts
    note: define .env variable for api URL

--- frontend/projects/auth/src/lib/services/auth.service.ts
+++ frontend/projects/auth/src/lib/services/auth.service.ts
@@ -1,57 +1,62 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { map, tap } from 'rxjs/operators';
-import { RegisterRequest, RegisterResponse } from '../../../../../src/app/app.interfaces';
+import { Injectable, signal } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import {
+  RegisterRequest,
+  RegisterResponse,
+} from '../../../../../src/app/app.interfaces';
 
 export interface ApplicationUser {
-	accessToken: string;
-	expiresIn: Date;
-	username: string;
+  accessToken: string;
+  expiresIn: Date;
+  username: string;
 }
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-  private currentUserSubject: BehaviorSubject<ApplicationUser | null>;
+  currentUser = signal<ApplicationUser | null>(null);
 
-  public currentUser: Observable<ApplicationUser | null>;
-
-  constructor(private http: HttpClient, private snackModal: MatSnackBar) { 
-    const userJson = localStorage.getItem('currentUser');
-    const user = userJson ? JSON.parse(userJson) : null;
-
-    this.currentUserSubject = new BehaviorSubject<ApplicationUser | null>(user);
-    this.currentUser = this.currentUserSubject.asObservable();
-  }
+  constructor(private http: HttpClient, private snackModal: MatSnackBar) {}
 
   public get currentUserValue(): ApplicationUser | null {
-    return this.currentUserSubject.value;
+    return this.currentUser();
   }
 
-  Login(email: string, password: string ){
-    return this.http.post<any>('/auth/login', { email, password }).pipe(map(user => {
-      if (user && user.accessToken) {
+  Login(email: string, password: string) {
+    return this.http.post<any>('http://localhost:3000/auth/login', { email, password }).pipe(
+      map((user) => {
+        if (user && user.accessToken) {
           localStorage.setItem('currentUser', JSON.stringify(user));
-          this.currentUserSubject.next(user);       
-      }
-      return user;
-    }));
-  }
-
-  Register(registerRequest: RegisterRequest): Observable<RegisterResponse> {
-    return this.http.post<RegisterResponse>('/api/auth/register', registerRequest).pipe(
-      tap((response: RegisterResponse) => 
-        this.snackModal.open('User added to database', 'Close', 
-        { duration: 1000, horizontalPosition: 'right', verticalPosition: 'top' })
-      )
+        }
+        return user;
+      })
     );
   }
 
-  Logout(){
+  retrieveUser(): void {
+    const user = localStorage.getItem('currentUser');
+    user ? this.currentUser.set(JSON.parse(user)) : this.currentUser.set(null);
+  }
+
+  Register(registerRequest: RegisterRequest): Observable<RegisterResponse> {
+    return this.http
+      .post<RegisterResponse>('http://localhost:3000/auth/register', registerRequest)
+      .pipe(
+        tap((response: RegisterResponse) =>
+          this.snackModal.open('User added to database', 'Close', {
+            duration: 1000,
+            horizontalPosition: 'right',
+            verticalPosition: 'top',
+          })
+        )
+      );
+  }
+
+  Logout() {
     localStorage.removeItem('currentUser');
-    this.currentUserSubject.next(null);
+    this.currentUser.set(null);
   }
 }
